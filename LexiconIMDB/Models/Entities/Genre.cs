55ab9d2ca4ab8d3cc01a9be60ce99d4b7015800f
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-07 01:24:48 +0100

    Add Entities

---
+++ LexiconIMDB/Models/Entities/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace LexiconIMDB.Models.Entities
+{
+    public enum Genre
+    {
+        Horror,
+        Thriller,
+        Comedy,
+        Drama,
+        Action
+    }
+}
---
+++ LexiconIMDB/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using LexiconIMDB.Models.Entities;
+
+namespace LexiconIMDB.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public float Rating { get; set; }
+    }
+}
