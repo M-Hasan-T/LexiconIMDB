09aa7d7cd836d2a6d29e8d3027422ec1c69a912f
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-11 01:01:00 +0100

    Index2 and Filter2

--- LexiconIMDB/Controllers/MoviesController.cs
+++ LexiconIMDB/Controllers/MoviesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LexiconIMDB.Data;
 using LexiconIMDB.Models.Entities;
+using LexiconIMDB.Models.ViewModels;
 
 namespace LexiconIMDB.Controllers
 {
@@ -38,6 +39,60 @@ namespace LexiconIMDB.Controllers
 
             return View(nameof(Index), await model.ToListAsync());
         }
+
+        public async Task<IActionResult> Filter2(IndexViewModel viewModel)
+        {
+            var movies = string.IsNullOrWhiteSpace(viewModel.Title) ?
+                                _context.Movie :
+                                _context.Movie.Where(m => m.Title.StartsWith(viewModel.Title));
+
+            movies = viewModel.Genre is null ?
+                                movies :
+                                movies.Where(m => m.Genre == viewModel.Genre);
+
+            var model = new IndexViewModel
+            {
+                Movies = await movies.ToListAsync(),
+                Genres = await GetGenresAsync()
+            };
+
+            return View(nameof(Index2), model);
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetGenresAsync()
+        {
+            return await _context.Movie.Select(m => m.Genre)
+                                .Distinct()
+                                .Select(g => new SelectListItem
+                                {
+                                    Text = g.ToString(),
+                                    Value = g.ToString()
+                                })
+                                .ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index2()
+        {
+            var movies = await _context.Movie.ToListAsync();
+
+            var model = new IndexViewModel
+            {
+                Movies = movies,
+                Genres = movies.Select(m => m.Genre)
+                                .Distinct()
+                                .Select(g => new SelectListItem
+                                {
+                                    Text = g.ToString(),
+                                    Value = g.ToString()
+                                })
+                                .ToList()
+            };
+
+            return View(model);
+        }
+
+
 
         // GET: Movies/Details/5
         [HttpGet]
---
+++ LexiconIMDB/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LexiconIMDB.Models.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LexiconIMDB.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Movie> Movies { get; set; } = new List<Movie>();
+        public IEnumerable<SelectListItem> Genres { get; set; } = new List<SelectListItem>();
+
+        public string? Title { get; set; }
+
+        public Genre? Genre { get; set; }
+    }
+}
